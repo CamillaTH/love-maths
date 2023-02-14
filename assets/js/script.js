7d8ff3393511ee7b7d803d44b4615c98cccc18cc
CamillaTH <hiltulacamilla@gmail.com>
2023-02-14 13:53:44 +0000

    added logic for division

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,10 @@ function runGame(gameType) {
     else if (gameType === "substact") {
         displaySubtractQuestion(num1, num2);
     }
+    else if (gameType === "division") {
+
+        displayDivisionQuestion(num1, num2);
+    }
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -58,9 +62,10 @@ function runGame(gameType) {
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
-
+    
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculateAnswer = calculateCorrectAnswer();
+    
     let isCorrect = userAnswer === calculateAnswer[0];
 
     if (isCorrect) {
@@ -93,6 +98,9 @@ function calculateCorrectAnswer() {
     }
     else if (operator === "-") {
         return [operand1 - operand2, "substact"];
+    }
+    else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     }
      else {
         alert(`Unimplemented operator ${operator}`);
@@ -138,4 +146,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1*operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
